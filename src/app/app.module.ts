6c9ab3f3fcfcdea92dd1794f9594c3778a9c0c83
Kesinee Boonchuay <bkasinee@hotmail.com>
2016-10-22 02:37:36 +0700

    Init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,34 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { StocksComponent } from './stocks.component';
+import { MutualfundsComponent } from './mutualfunds/mutualfunds.component';
+import { StockDirectiveDirective } from './stock-directive.directive';
+import { HighlightDirective } from './highlight.directive';
+import { StockService } from './stock.service';
+import { DateFormatterPipe } from './date-formatter.pipe';
+import { routing} from './app.routing';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { CurrencyService } from './currency.service';
+import { BoundsDirective } from './bounds.directive';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,StocksComponent,HighlightDirective,
+    MutualfundsComponent,
+    StockDirectiveDirective,
+    DateFormatterPipe,
+    DashboardComponent,
+    BoundsDirective
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    routing, NgbModule
   ],
-  providers: [],
+  providers: [StockService,CurrencyService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
